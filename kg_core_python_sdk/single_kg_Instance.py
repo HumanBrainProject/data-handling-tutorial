a58ec2e7bbb98af623f12da7f15c6053ada953e4
lzehl <l.zehl@fz-juelich.de>
2023-09-08 16:31:37 +0200

    updated script

--- kg_core_python_sdk/single_kg_Instance.py
+++ kg_core_python_sdk/single_kg_Instance.py
@@ -32,27 +32,55 @@ print()
 
 # for all key-value pairs defined by the metadata model, the key starts with a openMINDS vocabulary prefix
 openminds_prefix = "https://openminds.ebrains.eu/vocab/"
-openminds_metadata = {k: instance[k] for k in instance.keys() if k.startswith(openminds_prefix)}
+openminds_metadata = {k: instance[k] for k in instance if k.startswith(openminds_prefix)}
 for key, value in sorted(openminds_metadata.items()):
     print(f"{key}:", value)
 print()
 
 # for all key-value pairs additionally defined by the KG system, the key starts with another prefix
-kg_metadata = {k: instance[k] for k in instance.keys() if not k.startswith(openminds_prefix) and not k.startswith("@")}
+kg_metadata = {k: instance[k] for k in instance if not k.startswith(openminds_prefix) and not k.startswith("@")}
 for key, value in sorted(kg_metadata.items()):
     print(f"{key}:", value)
 print()
 
-# embedded instances (for embedded schema types)
+# nested (embedded) content is constraint by embedded schema types
+# such embedded content cannot be retrieved independent of the parent instance
+# and would also be automatically deleted with the deletion of the parent instance
 for key, value in sorted(openminds_metadata.items()):
-    if isinstance(value, dict) and "@type" in value.keys():
+    if isinstance(value, dict) and "@type" in value:
         print(key)
-        print(json.dumps(value, sort_keys=True, indent=2))
-    elif isinstance(value, list) and all(isinstance(i, dict) for i in value) and all("@type" in i.keys() for i in value):
+        for k, v in sorted(value.items()):
+            print(f"\t{k}:", v)
+        print()
+    elif isinstance(value, list) and all(isinstance(i, dict) for i in value) and all("@type" in i for i in value):
         print(key)
-        print(json.dumps(value, sort_keys=True, indent=2))
+        for i in value:
+            for k, v in sorted(i.items()):
+                print(f"\t{k}:", v)
+            print()
 
-
-# resolving edges (links to other instances)
-# TODO
-
+# related information is constrained by linked schema types
+# the links between instances (nodes) are called edges
+# the edge to another instance is defined by the uuid of the linked instance
+for key, value in sorted(openminds_metadata.items()):
+    if isinstance(value, dict) and "@id" in value:
+        print(key)
+        linked_instance_uuid = os.path.basename(value["@id"])
+        linked_instance = kg_client.instances.get_by_id(linked_instance_uuid).data
+        if linked_instance:
+            for k, v in sorted(linked_instance.items()):
+                print(f"\t{k}:", v)
+        else:
+            print("Instance not retrievable")
+        print()
+    elif isinstance(value, list) and all(isinstance(i, dict) for i in value) and all("@id" in i for i in value):
+        print(key)
+        for i in value:
+            linked_instance_uuid = os.path.basename(i["@id"])
+            linked_instance = kg_client.instances.get_by_id(linked_instance_uuid).data
+            if linked_instance:
+                for k, v in sorted(linked_instance.items()):
+                    print(f"\t{k}:", v)
+            else:
+                print("Instance not retrievable")
+            print()
